08d49ce5db7b07433c4602eafab4fcd3b476af99
abhishek789 <abhishek.somani@synerzip.com>
2015-04-07 11:12:08 +0530

    first commit

---
+++ config/config-dev.js
@@ -0,0 +1,6 @@
+
+module.exports = {
+  //db: 'mongodb://localhost/dashboard'
+    db: 'mongodb://sharadau:mongoforsh_1210@ds053251.mongolab.com:53251/dashboard'
+    //mongodb://<dbuser>:<dbpassword>@ds053251.mongolab.com:53251/dashboard
+};
